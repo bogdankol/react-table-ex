f4554b3d04259f06ae7b5ce69bbff8f9ac6bc1d4
bogdankol <68349689+bogdankol@users.noreply.github.com>
2026-01-19 22:20:30 +0200

    renamings

--- app/src/hooks/useDebouncedValue.ts
+++ app/src/hooks/useDebouncedValue.ts
@@ -1,17 +1,17 @@
 import { useEffect, useRef } from 'react'
 
-export function useDebouncedValue(value: string, delay: number, setFunc: (v: string) => void) {
-	let timer = useRef<ReturnType<typeof setTimeout> | null>(null)
+export function useDebouncedValue(value: string, delay: number, execFunc: (v: string) => void) {
+	const timer = useRef<ReturnType<typeof setTimeout> | null>(null)
 
 	useEffect(() => {
 		if (timer.current) clearTimeout(timer.current)
 		timer.current = setTimeout(() => {
-			setFunc(value)
+			execFunc(value)
 		}, delay)
 
 		return () => {
 			if (timer.current) clearTimeout(timer.current)
 		}
-	}, [delay, value])
+	}, [delay, value, execFunc])
 
 }
